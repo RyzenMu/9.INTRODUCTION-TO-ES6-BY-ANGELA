08f01558bb9f8a20520963f2ead0334fe8ef0917
RyzenMu <muruga13in2022@gmail.com>
2024-07-11 05:07:55 +0530

    my name

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 let myName = prompt('what is your name');
 let promptDiv = document.getElementsByClassName('.prompt');
-promptDiv.insertAdjacentHTML('afterend', `<p>Your name is ${myName}`);
+promptDiv.insertAdjacentHTML('afterend', `<p>Your name is ${myName}</P>`);
