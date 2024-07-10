fa562d45120995c2d6ac6d8357909fc279c8d695
RyzenMu <muruga13in2022@gmail.com>
2024-07-11 05:16:54 +0530

    no dot required

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myName = prompt('what is your name');
-let promptDiv = document.getElementsByClassName('.prompt');
-promptDiv.style.color = 'red';
-// promptDiv.insertAdjacentHTML('afterend', `<p>Your name is ${myName}</P>`);
+let promptDiv = document.getElementsByClassName('prompt');
+// promptDiv.style.color = 'red';
+promptDiv.insertAdjacentHTML('afterend', `<p>Your name is ${myName}</P>`);
 
