b43897a5386bfdd7ff7b7ca2cbac3555ff411d89
RyzenMu <muruga13in2022@gmail.com>
2024-07-11 05:20:35 +0530

    classname returns an array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let myName = prompt('what is your name');
-let promptDiv = document.getElementsByClassName('prompt');
+let promptDiv = document.getElementsByClassName('prompt')[0];
 promptDiv.style.color = 'red';
 promptDiv.insertAdjacentHTML('afterend', `<p>Your name is ${myName}</P>`);
 
