6133e85d53aba8d64f346751a4519232249656fb
RyzenMu <muruga13in2022@gmail.com>
2024-07-12 04:51:14 +0530

    length of name

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ let myName = prompt('what is your name');
 let promptDiv = document.getElementsByClassName('prompt')[0];
 promptDiv.style.color = 'red';
 promptDiv.insertAdjacentHTML('afterend', `<p>Your name is ${myName}</P>`);
+const lengthOfName = promptDiv.length;
+promptDiv.insertAdjacentHTML('afterend', `<h2>The length of your name is ${lengthOfName}</h2>`)
 
