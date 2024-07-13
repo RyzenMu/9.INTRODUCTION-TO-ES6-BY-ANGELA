d6408300bdc74974179ab8f4df9f70a6eac80d5f
RyzenMu <muruga13in2022@gmail.com>
2024-07-13 17:44:49 +0530

    slice completed

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ promptDiv.insertAdjacentHTML('afterend', `<p>Your name is ${myName}</P>`);
 const lengthOfName = myName.length;
 promptDiv.insertAdjacentHTML('afterend', `<h2>The length of your name is ${lengthOfName}</h2>`)
 
+// String Slice
+function sliceButton(){
+    const sliceString = prompt("Enter the string to be sliced");
+    const slicedString = sliceString.slice(0, 10);
+    document.querySelector('#sliceResult').innerText = slicedString;
+}
+
